a4977f596d7bf2e36e94532601e7ecc3efa5cf57
oztin <0315108515@miutt.edu.mx>
2023-12-10 08:25:11 -0800

    test(test): :test_tube: remove test

--- apps/auth/src/guards/roles/roles.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { RolesGuard } from '@management-auth/guards/roles/roles.guard';
-
-describe('RolesGuard', () => {
-  it('should be defined', () => {
-    expect(new RolesGuard()).toBeDefined();
-  });
-});
