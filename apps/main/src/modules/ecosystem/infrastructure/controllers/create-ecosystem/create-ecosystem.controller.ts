befe0201ca2ec45ebbe7649c37143545a030bbcc
oztin <roberto.austin@innovationsmi.com>
2023-12-22 23:27:48 -0800

    feat: :sparkles: new services added to create ecosystems
    MS-65

---
+++ apps/main/src/modules/ecosystem/infrastructure/controllers/create-ecosystem/create-ecosystem.controller.ts
@@ -0,0 +1,29 @@
+import { BadRequestException, Body, Controller, HttpException, Logger, Post } from '@nestjs/common';
+import { CreateEcosystemCommand, CreateEcosystemService } from '@module-eco/application';
+import { CreateEcosystemInputDto, CreateEcosystemOutputDto } from '@module-eco/infrastructure';
+
+@Controller('management-ecosystem/')
+export class CreateEcosystemController {
+  private readonly logger = new Logger(CreateEcosystemController.name);
+  constructor(private readonly createEcosystemService: CreateEcosystemService) {}
+
+  @Post()
+  async create(@Body() dto: CreateEcosystemInputDto): Promise<CreateEcosystemOutputDto> {
+    try {
+      const command = new CreateEcosystemCommand({
+        name: dto.name,
+        description: dto.description,
+        enabled: dto.enabled,
+      });
+
+      const domain = await this.createEcosystemService.process(command);
+
+      return new CreateEcosystemOutputDto(domain);
+    } catch (error) {
+      if (error instanceof HttpException) throw error;
+      const err = error as Error;
+      this.logger.error(err.message, err.stack);
+      throw new BadRequestException(err.message, err.name);
+    }
+  }
+}
---
+++ apps/main/src/modules/ecosystem/infrastructure/controllers/create-ecosystem/presentation/create-ecosystem-output.dto.ts
@@ -0,0 +1,29 @@
+interface ICreateEcosystemOutputDto {
+  id: string;
+  name: string;
+  description: string;
+  isEnabled: boolean;
+  createdAt: string | Date;
+  updatedAt: string | Date;
+  deletedAt: string | Date;
+}
+
+export class CreateEcosystemOutputDto implements ICreateEcosystemOutputDto {
+  id: string;
+  name: string;
+  description: string;
+  isEnabled: boolean;
+  createdAt: string | Date;
+  updatedAt: string | Date;
+  deletedAt: string | Date;
+
+  constructor(root: ICreateEcosystemOutputDto) {
+    this.id = root.id;
+    this.name = root.name;
+    this.description = root.description;
+    this.isEnabled = root.isEnabled;
+    this.createdAt = root.createdAt instanceof Date ? root.createdAt.toISOString() : root.createdAt;
+    this.updatedAt = root.updatedAt instanceof Date ? root.updatedAt.toISOString() : root.updatedAt;
+    this.deletedAt = root.deletedAt instanceof Date ? root.deletedAt.toISOString() : root.deletedAt;
+  }
+}
