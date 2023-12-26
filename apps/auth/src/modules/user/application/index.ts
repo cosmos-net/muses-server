90351148787397170e24ae9b6a58456f626b9eec
enriquesmi <enrique.chavez@innovationsmi.com>
2023-12-26 14:15:49 -0800

    addded rule on eslint

--- apps/auth/src/modules/user/application/index.ts
+++ apps/auth/src/modules/user/application/index.ts
@@ -1,6 +1,6 @@
-export * from '@app-auth/modules/user/application/use-cases/constants/injection-tokens';
 export * from '@app-auth/modules/user/application/use-cases/create-user-root/create-user-root.service';
 export * from '@app-auth/modules/user/application/use-cases/get-user/get-user.query';
 export * from '@app-auth/modules/user/application/use-cases/get-user/get-user.service';
 export * from '@app-auth/modules/user/application/use-cases/validate-password/validate-password.query';
 export * from '@app-auth/modules/user/application/use-cases/validate-password/validate-password.service';
+export * from '@app-auth/modules/user/application/constants/injection-tokens';
