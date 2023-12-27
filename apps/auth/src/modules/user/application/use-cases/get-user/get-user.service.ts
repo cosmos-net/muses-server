91eb9d4d3697793a4c16b7ab845b2cfa531e62a0
enriquesmi <enrique.chavez@innovationsmi.com>
2023-12-26 17:07:26 -0800

    changed imports and lintings

--- apps/auth/src/modules/user/application/use-cases/get-user/get-user.service.ts
+++ apps/auth/src/modules/user/application/use-cases/get-user/get-user.service.ts
@@ -1,18 +1,13 @@
-import { IApplicationServiceQuery } from '@lib-commons/application';
+import { IApplicationServiceQuery } from '@lib-commons/application/application-service-query';
 import { Inject, Injectable } from '@nestjs/common';
-import {
-  GetUserQuery,
-  USER_REPOSITORY,
-} from '@app-auth/modules/user/application';
-import { IUserRepository } from '@app-auth/modules/user/domain';
+import { GetUserQuery } from '@app-auth/modules/user/application/use-cases/get-user/get-user.query';
+import { IUserRepository } from '@app-auth/modules/user/domain/contracts/user-repository';
+import { USER_REPOSITORY } from '@app-auth/modules/user/application/constants/injection-tokens';
 import { User } from '@app-auth/modules/user/domain/user';
 
 @Injectable()
 export class GetUserService implements IApplicationServiceQuery<GetUserQuery> {
-  constructor(
-    @Inject(USER_REPOSITORY)
-    private userRepository: IUserRepository,
-  ) {}
+  constructor(@Inject(USER_REPOSITORY) private userRepository: IUserRepository) {}
 
   process<T extends GetUserQuery>(query: T): Promise<User> {
     const { email } = query;
