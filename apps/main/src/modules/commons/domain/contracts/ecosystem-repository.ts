b3825766f9944633b35558e5b73a35cd3615bdb9
Jair Guzman <jair.guzman@innovationsmi.com>
2024-01-11 02:22:08 -0800

    feat(ecosystems): :sparkles: feature to delete ecosystem

--- apps/main/src/modules/commons/domain/contracts/ecosystem-repository.ts
+++ apps/main/src/modules/commons/domain/contracts/ecosystem-repository.ts
@@ -7,4 +7,5 @@ export interface IEcosystemRepository {
   byIdOrFail(id: string): Promise<Ecosystem>;
   list(): Promise<ListEcosystem>;
   list(options: IPaginationOrder): Promise<ListEcosystem>;
+  delete(id: string): Promise<void>;
 }
