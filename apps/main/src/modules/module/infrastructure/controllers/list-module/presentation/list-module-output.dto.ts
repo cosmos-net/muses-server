d6bc646b8adf35d92f189c79291381361a337736
Jair Guzman <jair.guzman@innovationsmi.com>
2024-02-20 23:40:46 -0800

    feat(module-module): :sparkles: added GET endpoint for module list

---
+++ apps/main/src/modules/module/infrastructure/controllers/list-module/presentation/list-module-output.dto.ts
@@ -0,0 +1,50 @@
+import { IPagination } from '@lib-commons/domain/list/pagination/pagination';
+import { PageDto } from '@lib-commons/infrastructure/presentation/output-pagination/page.dto';
+import { PaginationMetadataDto } from '@lib-commons/infrastructure/presentation/output-pagination/pagination-metadata.dto';
+
+interface IListModuleOutputDto {
+  id?: string;
+  name: string;
+  description: string;
+  projectId: string;
+  isEnabled: boolean;
+  createdAt: string | Date;
+  updatedAt: string | Date;
+  deletedAt?: string | Date;
+}
+
+interface IListModuleOutputDtoResponse {
+  list: IListModuleOutputDto[];
+  total: number;
+}
+
+export class ListModuleOutputDto {
+  readonly items: IListModuleOutputDto[];
+  readonly meta?: PaginationMetadataDto;
+
+  constructor(items: IListModuleOutputDtoResponse, pagination: IPagination) {
+    const list = items.list.map((item) => {
+      return {
+        id: item.id,
+        name: item.name,
+        description: item.description,
+        projectId: item.projectId,
+        isEnabled: item.isEnabled,
+        createdAt: item.createdAt,
+        updatedAt: item.updatedAt,
+        deletedAt: item.deletedAt,
+      };
+    });
+
+    const paginationMetadataDto = new PaginationMetadataDto({
+      page: pagination.page,
+      limit: pagination.limit,
+      totalItems: items.total,
+    });
+
+    const { items: itemsPage, meta } = new PageDto<IListModuleOutputDto>(list, paginationMetadataDto);
+
+    this.items = itemsPage;
+    this.meta = meta;
+  }
+}
