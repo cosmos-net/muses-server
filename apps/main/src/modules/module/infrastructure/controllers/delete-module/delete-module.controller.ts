a0f0cd3fb546df2a46fa9ae6bfe62a1c2954ed3c
Jair Guzman <jair.guzman@innovationsmi.com>
2024-02-22 22:02:53 -0800

    feat(module-management): :sparkles: added endpoint to remove a module

---
+++ apps/main/src/modules/module/infrastructure/controllers/delete-module/delete-module.controller.ts
@@ -0,0 +1,38 @@
+import { Controller, Delete, Logger, Param } from '@nestjs/common';
+import { ExceptionManager } from '@lib-commons/domain/exception-manager';
+import { DeleteModuleService } from '@module-module/application/use-cases/delete-module/delete-module.service';
+import { DeleteModuleInputDto } from '@module-module/infrastructure/controllers/delete-module/presentation/delete-module-input.dto';
+import {
+  DeleteModuleOuputDto,
+  IDeleteModuleOutputDto,
+} from '@module-module/infrastructure/controllers/delete-module/presentation/delete-module-output.dto';
+import { DeleteModuleCommand } from '@module-module/application/use-cases/delete-module/delete-module.command';
+
+@Controller('module/')
+export class DeleteModuleController {
+  private readonly logger = new Logger(DeleteModuleController.name);
+  constructor(private readonly deleteModuleService: DeleteModuleService) {}
+
+  @Delete('/:id')
+  async delete(@Param() dto: DeleteModuleInputDto): Promise<IDeleteModuleOutputDto> {
+    try {
+      const command = new DeleteModuleCommand({
+        id: dto.id,
+      });
+
+      const result = await this.deleteModuleService.process(command);
+
+      const success = result ? (result ? true : false) : false;
+
+      const mapper = new DeleteModuleOuputDto({
+        success,
+        id: dto.id,
+      });
+
+      return mapper;
+    } catch (error) {
+      this.logger.error(error);
+      throw ExceptionManager.createSignatureError(error);
+    }
+  }
+}
