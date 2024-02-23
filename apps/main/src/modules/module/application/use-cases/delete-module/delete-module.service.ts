202be1f40b17694749ef0482bf51e86d47c28961
Jair Guzman <jair.guzman@innovationsmi.com>
2024-02-22 22:02:53 -0800

    feat(module-management): :sparkles: added endpoint to remove a module

---
+++ apps/main/src/modules/module/application/use-cases/delete-module/delete-module.service.ts
@@ -0,0 +1,30 @@
+import { IApplicationServiceCommand } from '@lib-commons/application/application-service-command';
+import { Inject, Injectable } from '@nestjs/common';
+import { DeleteModuleCommand } from '@module-module/application/use-cases/delete-module/delete-module.command';
+import { MODULE_REPOSITORY } from '@module-module/application/constants/injection-tokens';
+import { IModuleRepository } from '@module-module/domain/contracts/module-repository';
+import { ModuleNotFoundException } from '@module-project/domain/exceptions/module-not-found.exception';
+
+@Injectable()
+export class DeleteModuleService implements IApplicationServiceCommand<DeleteModuleCommand> {
+  constructor(
+    @Inject(MODULE_REPOSITORY)
+    private moduleRepository: IModuleRepository,
+  ) {}
+
+  async process<T extends DeleteModuleCommand>(command: T): Promise<number | undefined> {
+    const { id } = command;
+
+    const module = await this.moduleRepository.searchOneBy(id, {
+      withDeleted: true,
+    });
+
+    if (!module) {
+      throw new ModuleNotFoundException();
+    }
+
+    const result = await this.moduleRepository.softDeleteBy(module);
+
+    return result;
+  }
+}
