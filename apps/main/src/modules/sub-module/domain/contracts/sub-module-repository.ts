63a38b84db6de0627872041a852733763af3e47c
Jair Guzman <jair.guzman@innovationsmi.com>
2024-02-27 21:10:20 -0800

    feat(sub-module-management): :sparkles: added POST endpoint for submodule creation

--- apps/main/src/modules/sub-module/domain/contracts/sub-module-repository.ts
+++ apps/main/src/modules/sub-module/domain/contracts/sub-module-repository.ts
@@ -2,4 +2,7 @@ import { SubModule } from '@module-sub-module/domain/aggregate/sub-module';
 
 export interface ISubModuleRepository {
   searchOneBy(id: string, options: { withDeleted: boolean }): Promise<SubModule | null>;
+  isNameAvailable(name: string): Promise<boolean>;
+  persist(subModule: SubModule);
+  delete(id: string): Promise<void>;
 }
