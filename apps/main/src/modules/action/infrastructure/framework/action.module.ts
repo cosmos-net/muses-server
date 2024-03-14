40c1ca510709dbe9bb4036b27196e55d493fc5f0
Jair Guzman <jair.guzman@innovationsmi.com>
2024-03-13 21:37:35 -0700

    fix(action-module): :bug: update imports in action module

--- apps/main/src/modules/action/infrastructure/framework/action.module.ts
+++ apps/main/src/modules/action/infrastructure/framework/action.module.ts
@@ -4,10 +4,10 @@ import { GetActionController } from '@module-action/infrastructure/controllers/g
 import { GetActionService } from '@module-action/application/use-cases/get-action/ge-action.service';
 import { ACTION_REPOSITORY } from '@module-action/application/constants/injection-token';
 import { TypeOrmActionRepository } from '@module-action/infrastructure/repositories/typeorm-action.repository';
-import { Action } from '@module-action/domain/aggregate/action';
+import { ActionEntity } from '@module-action/infrastructure/domain/action-muses.entity';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([Action])],
+  imports: [TypeOrmModule.forFeature([ActionEntity])],
   controllers: [GetActionController],
   providers: [
     GetActionService,
