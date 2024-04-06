cbde0fa7a8d69e4a8115673f9b96fd364ede5df9
Jair Guzman <jair.guzman@innovationsmi.com>
2024-04-05 21:12:10 -0700

    test(resource-module-management): :test_tube: added e2e persistence tests for resource module

---
+++ apps/main/test/resource/update-resource/update-resource-test-psersistence.e2e.spec.ts
@@ -0,0 +1,78 @@
+/* eslint-disable hexagonal-architecture/enforce */
+import { ActionEntity } from '@module-action/infrastructure/domain/action-muses.entity';
+import { ResourceEntity } from '@module-resource/infrastructure/domain/resources-muses.entity';
+import { INestApplication, ValidationPipe } from '@nestjs/common';
+import { TestingModule } from '@nestjs/testing';
+import { getRepositoryToken } from '@nestjs/typeorm';
+import { ModuleFactory } from '@test-muses/utils/config/module-factory';
+import { createActionGenerator } from '@test-muses/utils/generators/action.generator';
+import { createResourceGenerator } from '@test-muses/utils/generators/resource.generator';
+import * as request from 'supertest';
+import { MongoRepository } from 'typeorm';
+
+describe('Update resource test persistence (e2e)', () => {
+  let moduleFixture: TestingModule;
+  let app: INestApplication;
+
+  let resourceRepository: MongoRepository<ResourceEntity>;
+  let actionRepository: MongoRepository<ActionEntity>;
+
+  beforeAll(async () => {
+    moduleFixture = await ModuleFactory.createModule();
+
+    app = moduleFixture.createNestApplication();
+
+    app.useGlobalPipes(new ValidationPipe({ transform: true, whitelist: true, forbidNonWhitelisted: true }));
+
+    resourceRepository = moduleFixture.get<MongoRepository<ResourceEntity>>(getRepositoryToken(ResourceEntity));
+    actionRepository = moduleFixture.get<MongoRepository<ActionEntity>>(getRepositoryToken(ActionEntity));
+
+    await app.init();
+
+    await resourceRepository.delete({});
+    await actionRepository.delete({});
+  });
+
+  afterAll(async () => {
+    await resourceRepository.delete({});
+    await actionRepository.delete({});
+    if (app) await app.close();
+  });
+
+  beforeEach(async () => {
+    await resourceRepository.delete({});
+    await actionRepository.delete({});
+  });
+
+  jest.setTimeout(99999999);
+
+  describe('Given we want to update a resource', () => {
+    describe('When we send a list of actions and triggers valid', () => {
+      test('Then expect to update the resource', async () => {
+        const action = await createActionGenerator(actionRepository);
+        const resource = await createResourceGenerator(resourceRepository, {
+          actions: [action._id],
+        });
+
+        const action2 = await createActionGenerator(actionRepository);
+        const resource2 = await createResourceGenerator(resourceRepository);
+
+        const params = {
+          id: resource._id.toHexString(),
+          actions: [action2._id.toHexString()],
+          triggers: [resource2._id.toHexString()],
+        };
+
+        const response = await request(app.getHttpServer())
+          .patch('actions')
+          .send({ id: params.id, actions: params.actions, triggers: params.triggers });
+
+        expect(response.status).toBe(200);
+        expect(response.body.name).toBe(resource.name);
+        expect(response.body.description).toBe(resource.description);
+        expect(response.body.actions[0]).toBe(action2._id.toHexString());
+        expect(response.body.triggers[0]).toBe(resource2._id.toHexString());
+      });
+    });
+  });
+});
--- apps/main/test/utils/entities.ts
+++ apps/main/test/utils/entities.ts
@@ -3,5 +3,7 @@ import { ModuleEntity } from '@module-module/infrastructure/domain/module-muses.
 import { SubModuleEntity } from '@module-sub-module/infrastructure/domain/sub-module-muses.entity';
 import { EcosystemEntity } from '@module-eco/infrastructure/domain/ecosystem-muses.entity';
 import { ProjectEntity } from '@module-project/infrastructure/domain/project-muses.entity';
+import { ActionEntity } from '@module-action/infrastructure/domain/action-muses.entity';
+import { ResourceEntity } from '@module-resource/infrastructure/domain/resources-muses.entity';
 
-export const entities = [EcosystemEntity, ProjectEntity, ModuleEntity, SubModuleEntity];
+export const entities = [EcosystemEntity, ProjectEntity, ModuleEntity, SubModuleEntity, ActionEntity, ResourceEntity];
