7ad0bdb3f33d96100db6ada6c8d2b59995b4fe2b
oztin <roberto.austin@innovationsmi.com>
2024-04-14 21:34:18 -0700

    fix(resource-module-management): :bug: fix resource enabled issue

--- apps/main/src/modules/resource/infrastructure/controllers/update-resource/presentation/update-resource-input.dto.ts
+++ apps/main/src/modules/resource/infrastructure/controllers/update-resource/presentation/update-resource-input.dto.ts
@@ -1,16 +1,6 @@
 import { EnumMethodValue } from '@module-resource/domain/aggregate/value-objects/method.vo';
 import { IsObjectIdHex } from '@lib-commons/infrastructure/helpers/custom-validators/object-id-hex';
-import {
-  ArrayMinSize,
-  IsArray,
-  IsBoolean,
-  IsEnum,
-  IsNotEmpty,
-  IsOptional,
-  IsString,
-  IsUrl,
-  Length,
-} from 'class-validator';
+import { IsArray, IsBoolean, IsEnum, IsNotEmpty, IsOptional, IsString, IsUrl, Length } from 'class-validator';
 import { Transform } from 'class-transformer';
 
 export class UpdateResourceInputDto {
@@ -52,13 +42,12 @@ export class UpdateResourceInputDto {
   @IsString({ each: true })
   @IsArray()
   @IsOptional()
-  @IsOptional()
   readonly triggers?: string[] = [];
 
   @IsObjectIdHex()
   @IsString({ each: true })
   @IsArray()
-  @ArrayMinSize(1)
+  @IsNotEmpty({ each: true, message: 'actions must not be empty' })
   @IsOptional()
   readonly actions?: string[] = [];
 }
