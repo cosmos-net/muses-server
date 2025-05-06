8bae751e52c536995daf2cf23871e5f33531d3d5
2314-RAustin <0315108515@miutt.edu.mx>
2025-05-05 18:17:53 -0700

    feat: add global setup script for Jest testing configuration

---
+++ test/scripts/jest-global-setup.ts
@@ -0,0 +1,6 @@
+// Este archivo se ejecuta una vez antes de todas las pruebas
+// Es utilizado para configuración global de pruebas
+module.exports = async (): Promise<void> => {
+  // Realizar cualquier configuración necesaria antes de que se ejecuten las pruebas
+  console.log('Configuración global de pruebas inicializada');
+};
