996b6b90409f7b239c9f048b87b49f3629879181
2314-RAustin <0315108515@miutt.edu.mx>
2025-05-05 17:23:53 -0700

    refactor(main): :recycle: Simplify bootstrap process by removing unused imports and using NATS_URL for server configuration

--- src/modules/core/infrastructure/framework/main.ts
+++ src/modules/core/infrastructure/framework/main.ts
@@ -1,9 +1,6 @@
 import { NestFactory } from '@nestjs/core';
-import { ConfigService } from '@nestjs/config';
-import { Logger, ValidationPipe } from '@nestjs/common';
+import { ValidationPipe } from '@nestjs/common';
 import { MainModule } from '@core/infrastructure/framework/main.module';
-import { ClientType } from '@core/domain/contracts/types/var-environment-map/client/client.type';
-import { ServerMainType } from '@core/domain/contracts/types/var-environment-map/servers/server-main.type';
 import { TransformInterceptor } from '@core/infrastructure/framework/transform.interceptor';
 import { ValidationPipeWithExceptionFactory } from '@core/infrastructure/framework/global-validation.pipe';
 import { MicroserviceExceptionFilter } from '@core/infrastructure/framework/microservice-exception.filter';
@@ -11,41 +8,22 @@ import { MicroserviceOptions, Transport } from '@nestjs/microservices';
 import { HttpExceptionFilter } from '@core/infrastructure/framework/http-exception.filter';
 
 async function bootstrap() {
+  const natsURL = process.env.NATS_URL;
+
+  if (!natsURL) {
+    throw new Error('NATS_URL is not defined');
+  }
+
   const app = await NestFactory.createMicroservice<MicroserviceOptions>(MainModule, {
     transport: Transport.NATS,
     options: {
-      servers: ['nats://localhost:4223'],
+      servers: [natsURL],
     },
   });
-
-  const configService = app.get(ConfigService);
-  const client = configService.get<ClientType>('client');
-  const server = configService.get<ServerMainType>('main');
-
-  if (!server) {
-    throw new Error('Server main is not defined');
-  }
-
-  if (!client) {
-    throw new Error('Client is not defined');
-  }
 
   app.useGlobalPipes(new ValidationPipeWithExceptionFactory(), new ValidationPipe({ forbidUnknownValues: true }));
   app.useGlobalInterceptors(new TransformInterceptor());
   app.useGlobalFilters(new MicroserviceExceptionFilter(), new HttpExceptionFilter());
-  // app.setGlobalPrefix('api/v1/muses-management/');
-
-  const origin = server.host === '127.0.0.1' ? `${client.protocol}://${client.host}:${client.port}` : `${client.protocol}://${client.host}`;
-
-  // app.enableCors({
-  //   origin,
-  //   methods: 'GET,POST,PUT,DELETE,PATCH',
-  //   credentials: true,
-  // });
-
-  const port = process.env.PORT ?? server.port;
-
-  // await app.listen(port, () => Logger.log(`Running on port ${port}`, server.name));
 
   await app.listen();
 }
