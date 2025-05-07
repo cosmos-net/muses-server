07d096d799de51031e83fa8bc62189120165202b
2314-RAustin <0315108515@miutt.edu.mx>
2025-05-06 17:22:44 -0700

    refactor: remove outdated sub-module tests and related configurations
    - Deleted various end-to-end tests for sub-modules including delete, get, list, and update functionalities.
    - Removed associated test utilities and factories for generating test entities.
    - Cleaned up configuration files related to MongoDB testing setup.

--- test/utils/config/mongo-test.loader.ts
+++
@@ -1,6 +0,0 @@
-// eslint-disable-next-line hexagonal-architecture/enforce
-import { registerAs } from '@nestjs/config';
-import { MongoTestType } from './mongo-test.type';
-import { MongoTestConfigLoader } from './mongo-config.loader';
-
-export const mongo_test_loader = registerAs('mongo_test', (): MongoTestType => MongoTestConfigLoader().mongo_test);
